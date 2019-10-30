392d97ea0f9021710c8cf901facfe3bbf372fb17
Vitor Norton <norton.tavares@lambda3.com.br>
2019-10-30 15:00:26 -0300

    changed project to typescript

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import * as React from 'react'
+import 'heartthrob'
+
+const Button = () => {
+	return (
+		<button className='btn'>
+			Button text
+		</button>
+	)
+}
+
+export default Button
