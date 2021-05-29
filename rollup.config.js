ca0c2f863136eb0de5547c65bf3da9d76cba5b54
Vítor Norton <vtnorton@outlook.com>
2021-05-29 15:59:46 -0300

    Fixing build erros

--- rollup.config.js
+++ rollup.config.js
@@ -9,32 +9,39 @@ import dts from 'rollup-plugin-dts'
 
 const packageJson = require('./package.json')
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: packageJson.types,
-      format: 'es',
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    dts(),
-    typescript(),
-    postcss({
-      extensions: ['.css'],
-    }),
-  ],
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      postcss({
+        extensions: ['.css'],
+      }),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.types,
+        format: 'es',
+      },
+    ],
+    plugins: [dts()],
+  },
+]
