b9694c39be518c9a09e4d22559c58135f21e2bad
Vitor Norton <vtnorton@outlook.com>
2021-07-04 19:17:09 -0300

    added notInformedTreatment

--- src/extensions/stringExtensions.spec.tsx
+++ src/extensions/stringExtensions.spec.tsx
@@ -1,4 +1,4 @@
-import { getIntials, notInformedTextTreatment, removeSpecialCharacters } from './stringExtensions'
+import { getIntials, notInformedTextTreatment, notInformedTreatment, removeSpecialCharacters } from './stringExtensions'
 
 describe('Remove special characters', () => {
 	it('should return string with no special characters', () => {
@@ -38,20 +38,39 @@ describe('Get initials', () => {
 		expect(result).not.toBe(final)
 	})
 
-	describe('notInformadTreatment', () => {
-		it('should return NotInformed text when no data is passed', () => {
-			const values = [null, undefined, '']
-			values.forEach(element => {
-				const result = notInformedTextTreatment(element)
-				expect(result).toBe('Não informado.')
-			})
-		})
+})
 
-		it('should return text when it is passed', () => {
-			const value = 'Lorem ipsum dolor siet'
-			const result = notInformedTextTreatment(value)
-			
-			expect(result).toBe(value)
+describe('notInformadTreatment', () => {
+	it('should return NotInformed text when no data is passed', () => {
+		const values = [null, undefined, '']
+		values.forEach(element => {
+			const result = notInformedTextTreatment(element)
+			expect(result).toBe('Não informado.')
 		})
+	})
+
+	it('should return text when it is passed', () => {
+		const value = 'Lorem ipsum dolor siet'
+		const result = notInformedTextTreatment(value)
+
+		expect(result).toBe(value)
+	})
+})
+
+describe('Select approprieted treatment', () => {
+	it('should select date treatement when passing date', () => {
+		const value = new Date(2020, 2, 30, 15, 16, 5)
+
+		const result = notInformedTreatment(value)
+
+		expect(result).toBe('3/30/2020')
+	})
+
+	it('should select text treatement when passing string', () => {
+		const value = 'Hey there'
+
+		const result = notInformedTreatment(value)
+
+		expect(result).toBe(value)
 	})
 })
--- src/extensions/stringExtensions.tsx
+++ src/extensions/stringExtensions.tsx
@@ -1,12 +1,14 @@
 export const removeSpecialCharacters = (text: string): string => {
-	return text
-		.toLowerCase()
-		.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a')
-		.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e')
-		.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i')
-		.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o')
-		.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u')
-		.replace(new RegExp('[Ç]', 'gi'), 'c')
+	if (text)
+		return text
+			.toLowerCase()
+			.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a')
+			.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e')
+			.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i')
+			.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o')
+			.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u')
+			.replace(new RegExp('[Ç]', 'gi'), 'c')
+	return ''
 }
 
 export const getIntials = (value: string): string => {
@@ -18,15 +20,22 @@ export const getIntials = (value: string): string => {
 }
 
 export const notInformedDateTreatment = (date: Date | null | undefined): string => {
-	if(date === undefined || date?.toString() === '0001-01-01T00:00:00' )
+	if (date === undefined || date?.toString() === '0001-01-01T00:00:00')
 		return 'Não informado.'
-		
+
 	return new Date(date as Date).toLocaleDateString()
 }
 
 export const notInformedTextTreatment = (text: string | null | undefined): string => {
-	if (text === null || text === '' || text === undefined) 
+	if (text === null || text === '' || text === undefined)
 		return 'Não informado.'
-	
+
 	return text
+}
+
+export const notInformedTreatment = (item: any) => {
+	if (item instanceof Date)
+		return notInformedDateTreatment(item)
+
+	return notInformedTextTreatment(item)
 }
