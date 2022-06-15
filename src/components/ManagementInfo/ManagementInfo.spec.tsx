659ea87144125acbec64fe7005613300e35ccc60
Vitor Norton <vtnorton@outlook.com>
2022-06-14 22:50:26 -0300

    ajuste no test

--- src/components/ManagementInfo/ManagementInfo.spec.tsx
+++ src/components/ManagementInfo/ManagementInfo.spec.tsx
@@ -4,29 +4,30 @@ import ManagementInfo from './ManagementInfo'
 import { ManagementInfoProps } from './ManagementInfoTypes'
 import { renderComponent } from './../../infrastructure/test-helpers/test-renderer'
 
+const date = new Date()
+const item: ManagementInfoProps = {
+	item: {
+		createdIn: date,
+		createdBy: 'Vitor Norton',
+		updatedIn: date,
+		updatedBy: 'Vitor Norton',
+	},
+}
+const newUpdatedIn = new Date(item.item.createdIn.getTime() + (1000 * 60 * 60 * 24))
+const newUpdatedBy = item.item.updatedBy = 'Christine Daee'
+const testItem: ManagementInfoProps = {
+	item: {
+		createdIn: item.item.createdIn,
+		createdBy: item.item.createdBy,
+		updatedIn: newUpdatedIn,
+		updatedBy: newUpdatedBy,
+	},
+}
 
-const componentRender = () => {
-	const date = new Date()
-	const item: ManagementInfoProps = {
-		item: {
-			createdIn: date,
-			createdBy: 'Vitor Norton',
-			updatedIn: date,
-			updatedBy: 'Vitor Norton',
-		},
-	}
-	const newUpdatedIn = new Date(item.item.createdIn.getTime() + (1000 * 60 * 60 * 24))
-	const newUpdatedBy = item.item.updatedBy = 'Christine Daee'
-	const newItem: ManagementInfoProps = {
-		item: {
-			createdIn: item.item.createdIn,
-			createdBy: item.item.createdBy,
-			updatedIn: newUpdatedIn,
-			updatedBy: newUpdatedBy,
-		},
-	}
+const componentRender = (itemToTest: ManagementInfoProps = testItem) => {
 
-	const renderResult = renderComponent(<ManagementInfo item={newItem.item} />)
+
+	const renderResult = renderComponent(<ManagementInfo item={itemToTest.item} />)
 	return {
 		...renderResult,
 	}
